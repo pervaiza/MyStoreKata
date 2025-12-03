167898dc4fc9a7874a5eacc6425e3a1c1fba1ad4
pervaiza <m.akhtar@hotmail.co.uk>
2025-12-03 17:21:41 +0000

    Added Theory and InlineData to verify various test cases for single / multi items discount

--- MyStoreKata/MyStoreKata.UnitTests/CheckoutTests.cs
+++ MyStoreKata/MyStoreKata.UnitTests/CheckoutTests.cs
@@ -1,4 +1,5 @@
-﻿using MyStore.Domain;
+﻿using Xunit;
+using MyStore.Domain;
 using MyStoreKata.Services;
 
 namespace MyStoreKata.UnitTests
@@ -36,6 +37,23 @@ namespace MyStoreKata.UnitTests
             
             Assert.Throws<ArgumentException>(() => checkout.Scan(null));
             Assert.Throws<ArgumentException>(() => checkout.Scan(string.Empty));
+        }
+
+        [Theory]
+        [InlineData(new[] { "A", "B", "C" }, 100)]
+        [InlineData(new[] { "A", "A", "A" }, 130)]
+        [InlineData(new[] { "B", "B", "B" }, 75)]
+        [InlineData(new[] { "A", "A", "A", "B", "B", "C" }, 195)]
+        [InlineData(new[] { "D", "D", "D", "D" }, 60)]
+        public void GetTotalPrice_WithVariousItems_ReturnsExpectedTotal(string[] items, int expectedTotal)
+        {
+            var checkout = new Checkout(DefaultPricing.GetDefaultPricingRules());
+            foreach (var item in items)
+            {
+                checkout.Scan(item);
+            }
+            var totalPrice = checkout.GetTotalPrice();
+            Assert.Equal(expectedTotal, totalPrice);
         }
     }
 }
