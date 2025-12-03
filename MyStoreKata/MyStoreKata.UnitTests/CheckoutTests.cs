e2caa83e72c4150e9a24b7fb6e76b531dffbe7db
pervaiza <m.akhtar@hotmail.co.uk>
2025-12-03 16:29:15 +0000

    added initial projs

---
+++ MyStoreKata/MyStoreKata.UnitTests/CheckoutTests.cs
@@ -0,0 +1,13 @@
+﻿using MyStoreKata.Services;
+
+namespace MyStoreKata.UnitTests
+{
+    public class CheckoutTests
+    {
+        [Fact]
+        public void ThrowsNotImplementedException()
+        {
+            Assert.Throws<NotImplementedException>(() => new Checkout().Scan("A"));
+        }
+    }
+}
---
+++ MyStoreKata/MyStoreKata/Services/ICheckout.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyStoreKata.Services
+{
+    public interface ICheckout
+    {
+        void Scan(string item);
+
+        int GetTotalPrice();
+    }
+
+    public class Checkout : ICheckout
+    {
+        public int GetTotalPrice()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Scan(string item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
