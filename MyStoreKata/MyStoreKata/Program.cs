3edc444181ab5865d9d26ea137cfa66df93bc782
pervaiza <m.akhtar@hotmail.co.uk>
2025-12-03 17:35:23 +0000

    Updated program: CS to display the basket and calculate the basket price.

--- MyStoreKata/MyStoreKata/Program.cs
+++ MyStoreKata/MyStoreKata/Program.cs
@@ -1,2 +1,21 @@
 ﻿
-Console.WriteLine("Checkout");
+using MyStore.Domain;
+using MyStoreKata.Services;
+
+///<summary>
+///https://github.com/brightsg/checkout-kata
+/// </summary>
+
+var checkout = new Checkout(DefaultPricing.GetDefaultPricingRules());
+
+var myBasket = new[] { "A", "A", "A", "B", "B", "C" };
+
+foreach (var item in myBasket)
+{
+    checkout.Scan(item);
+}
+
+var totalPrice = checkout.GetTotalPrice();
+
+Console.WriteLine("Items in your basket: " + string.Join(", ", myBasket));
+Console.WriteLine($"Mr. Bob, the total amount due: {totalPrice}");
