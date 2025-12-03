67aa773ac69e80395676622c2b84df3e70d83184
pervaiza <m.akhtar@hotmail.co.uk>
2025-12-03 17:10:00 +0000

    Updated Scan and GetTotalPrice

--- MyStoreKata/MyStoreKata.UnitTests/CheckoutTests.cs
+++ MyStoreKata/MyStoreKata.UnitTests/CheckoutTests.cs
@@ -1,13 +1,41 @@
-﻿using MyStoreKata.Services;
+﻿using MyStore.Domain;
+using MyStoreKata.Services;
 
 namespace MyStoreKata.UnitTests
 {
     public class CheckoutTests
     {
         [Fact]
-        public void ThrowsNotImplementedException()
+        public void Constructor_WithNullPricingRules_ThrowsArgumentNullException()
         {
-            Assert.Throws<NotImplementedException>(() => new Checkout().Scan("A"));
+            Assert.Throws<ArgumentNullException>(() => new Checkout(null));
+        }
+
+        [Fact]
+        public void GetTotalPrice_NoItemsScanned_ReturnsZero()
+        {
+            var checkout = new Checkout(DefaultPricing.GetDefaultPricingRules());
+           
+            var totalPrice = checkout.GetTotalPrice();
+            
+            Assert.Equal(0, totalPrice);
+        }
+
+        [Fact]
+        public void Scan_WithUnknownItem_ThrowsArgumentException()
+        {
+            var checkout = new Checkout(DefaultPricing.GetDefaultPricingRules());
+            
+            Assert.Throws<ArgumentException>(() => checkout.Scan("X"));
+        }
+
+        [Fact]
+        public void Scan_WithNullOrEmptyItem_ThrowsArgumentException()
+        {
+            var checkout = new Checkout(DefaultPricing.GetDefaultPricingRules());
+            
+            Assert.Throws<ArgumentException>(() => checkout.Scan(null));
+            Assert.Throws<ArgumentException>(() => checkout.Scan(string.Empty));
         }
     }
 }
--- MyStoreKata/MyStoreKata/Services/ICheckout.cs
+++ MyStoreKata/MyStoreKata/Services/ICheckout.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-
+using MyStore.Domain;
 namespace MyStoreKata.Services
 {
     public interface ICheckout
@@ -13,14 +13,68 @@ namespace MyStoreKata.Services
 
     public class Checkout : ICheckout
     {
-        public int GetTotalPrice()
+        private readonly Dictionary<string, PricingRule> _pricingRules;
+        private readonly Dictionary<string, int> _scannedItems;
+
+        public Checkout(IEnumerable<PricingRule> pricingRules)
         {
-            throw new NotImplementedException();
+            if (pricingRules == null)
+                throw new ArgumentNullException(nameof(pricingRules));
+
+            _pricingRules = pricingRules.ToDictionary(rule => rule.Sku, rule => rule);
+
+            _scannedItems = new Dictionary<string, int>();
         }
 
         public void Scan(string item)
         {
-            throw new NotImplementedException();
+            if (string.IsNullOrWhiteSpace(item))
+                throw new ArgumentException("Item cannot be null or empty", nameof(item));
+
+            if (!_pricingRules.ContainsKey(item))
+                throw new ArgumentException($"Unknown item: {item}", nameof(item));
+
+            if (_scannedItems.ContainsKey(item))
+                _scannedItems[item]++;
+            else
+                _scannedItems[item] = 1;
+        }
+
+        public int GetTotalPrice()
+        {
+            int total = 0;
+
+            foreach (var scannedItem in _scannedItems)
+            {
+                string sku = scannedItem.Key;
+                int quantity = scannedItem.Value;
+
+                var pricingRule = _pricingRules[sku];
+
+                total += CalculatePrice(pricingRule, quantity);
+            }
+
+            return total;
+        }
+
+        private int CalculatePrice(PricingRule pricingRule, int quantity)
+        {
+            if (pricingRule.SpecialOffer == null)
+            {
+                return pricingRule.UnitPrice * quantity;
+            }
+
+            int specialOfferQuantity = pricingRule.SpecialOffer.Quantity;
+            int specialOfferPrice = pricingRule.SpecialOffer.Price;
+
+            int numberOfSpecialOffers = quantity / specialOfferQuantity;
+            int remainingItems = quantity % specialOfferQuantity;
+
+            int specialOfferTotal = numberOfSpecialOffers * specialOfferPrice;
+            int regularPriceTotal = remainingItems * pricingRule.UnitPrice;
+
+            return specialOfferTotal + regularPriceTotal;
         }
     }
+
 }
